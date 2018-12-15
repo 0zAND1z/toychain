33ae3306e41791e7c7efb18b93ac4dcd8e489bfc
Ganesh Prasad Kumble <kggp1995@users.noreply.github.com>
2018-12-15 12:52:54 +0530

    - PHASE:2 tcp support & testing 5s blocktime

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -30,6 +33,7 @@ type Message struct {
 }
 
 var Blockchain []Block
+var bcServer chan []Block
 
 func calculateHash(block Block) string {
 	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
@@ -144,16 +148,75 @@ func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	err := godotenv.Load()
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	bcServer = make(chan []Block)
+
+	t := time.Now()
+	genesisBlock := Block{0, t.String(), 0, "", ""}
+	spew.Dump(genesisBlock)
+	Blockchain = append(Blockchain, genesisBlock)
+
+	server, err := net.Listen("tcp", ":"+os.Getenv("ADDR"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer server.Close()
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConn(conn)
+
+	}
+
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	io.WriteString(conn, "Enter a new BPM: ")
+	scanner := bufio.NewScanner(conn)
+
 	go func() {
-		t := time.Now()
-		genesisBlock := Block{0, t.String(), 0, "", ""}
-		spew.Dump(genesisBlock)
-		Blockchain = append(Blockchain, genesisBlock)
+		for scanner.Scan() {
+			bpm, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Printf("%+v not a number: %v", scanner.Text(), err)
+				continue
+			}
+
+			newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], bpm)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+				newBlockchain := append(Blockchain, newBlock)
+				replaceChain(newBlockchain)
+			}
+			bcServer <- Blockchain
+			io.WriteString(conn, "\nEnter a new BPM: ")
+		}
 	}()
-	log.Fatal(run())
+
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+			output, err := json.Marshal(Blockchain)
+			if err != nil {
+				log.Fatal(err)
+			}
+			io.WriteString(conn, string(output))
+		}
+	}()
+
+	for _ = range bcServer {
+		spew.Dump(Blockchain)
+	}
 }
